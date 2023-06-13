f209739087cb4ef631c1c17fecd161c0e01bc752
Abolarin muyiwa <asquareblrn9@gmail.com>
2023-06-13 19:26:35 +0100

    correct home link

--- src/components/AddBlog.js
+++ src/components/AddBlog.js
@@ -85,10 +85,8 @@ navigate('/login')
                                 </div>
 
 
-                                <button type="submit" className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-blue-700 dark:focus:ring-primary-500">Sign up</button>
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Already have an account? <a href="/" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</a>
-                                </p>
+                                <button type="submit" className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-blue-700 dark:focus:ring-primary-500">Post</button>
+                               
                             </form>
                         </div>
                     </div>
