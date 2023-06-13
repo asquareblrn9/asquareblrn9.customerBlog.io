38aab86794276235f25e810daaec6e2bbbc16166
Abolarin muyiwa <asquareblrn9@gmail.com>
2023-06-13 19:33:27 +0100

    correct home link

--- src/components/AddBlog.js
+++ src/components/AddBlog.js
@@ -10,7 +10,7 @@ const AddBlog = () => {
     const navigate = useNavigate()
     //checking if user is logged in
     useEffect(()=>{
-if(!auth){
+if(!auth.currentUser){
 navigate('/login')
     }
     },[])
--- src/components/Navblog.js
+++ src/components/Navblog.js
@@ -11,7 +11,9 @@ const Navblog = () => {
       navigate('/')
     })
   }
-  console.log(auth.currentUser)
+
+  console.log(auth)
+
   return (
     <div>
       
@@ -31,7 +33,7 @@ const Navblog = () => {
           <a href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
         </li>
        
-        { auth ? 
+        { auth.currentUser != null ? 
         <>
         <li>
                   <a href="/createBlog" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Create Blog</a>
